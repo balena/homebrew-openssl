f10fc64bbb7ed292635b9a36b7cfdb3e24f190f4
Guilherme Balena Versiani <guibv@nymgo.com>
2019-08-30 16:52:58 -0300

    Forked botan 2.11.0 as well, depending on openssl@1.1 from here and adding debug symbols as well

---
+++ HomebrewFormula/botan.rb
@@ -0,0 +1,39 @@
+class Botan < Formula
+  desc "Cryptographic algorithms and formats library in C++"
+  homepage "https://botan.randombit.net/"
+  url "https://botan.randombit.net/releases/Botan-2.11.0.tar.xz"
+  sha256 "f7874da2aeb8c018fd77df40b2137879bf90b66f5589490c991e83fb3e8094be"
+  revision 1
+  head "https://github.com/randombit/botan.git"
+
+  bottle do
+  end
+
+  depends_on "pkg-config" => :build
+  depends_on "balena/with-dsym/openssl@1.1"
+
+  def install
+    ENV.cxx11
+
+    args = %W[
+      --prefix=#{prefix}
+      --docdir=share/doc
+      --cpu=#{MacOS.preferred_arch}
+      --cc=#{ENV.compiler}
+      --os=darwin
+      --with-openssl
+      --with-zlib
+      --with-bzip2
+      --with-debug-info
+    ]
+
+    system "./configure.py", *args
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.txt").write "Homebrew"
+    (testpath/"testout.txt").write Utils.popen_read("#{bin}/botan base64_enc test.txt")
+    assert_match "Homebrew", shell_output("#{bin}/botan base64_dec testout.txt")
+  end
+end
