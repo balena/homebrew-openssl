b236507962e86fe80ef29a0e69dd045648634985
Guilherme Balena Versiani <guibv@nymgo.com>
2019-08-31 00:12:40 -0300

    Stripping also botan

--- HomebrewFormula/botan.rb
+++ HomebrewFormula/botan.rb
@@ -31,6 +31,7 @@ class Botan < Formula
     system "./configure.py", *args
     system "make", "install"
     system "dsymutil", "#{lib}/libbotan-2.11.11.0.dylib", "-o", "#{lib}/libbotan-2.11.11.0.dylib.dSYM"
+    system "strip", "-x", "#{lib}/libbotan-2.11.11.0.dylib"
   end
 
   test do
